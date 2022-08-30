a999d3914111795ac2d499ab262da5816c8fbef0
Devang Bhuva <devangbhuva97@gmail.com>
2022-08-30 13:22:02 +0530

    Added tailwind css & remove bootstrap

--- components/footer.js
+++ components/footer.js
@@ -8,26 +8,26 @@ export default function Footer() {
         <div className='footer_copy'>
           <div className='footer_data'>
             <h3>Our Office</h3>
-            <div className='d-flex'>
+            <div className='flex'>
               <Image src="/icon/address.svg" alt="" width={16} height={22} />
               <p className='fix_address_w'>Plot No-1, Mahashakti Estate, Nr., Rajlakshmi Plot, Bhavda, Ahmedabad-382433. (Gujarat) India.</p>
             </div>
-            <div className='d-flex'>
+            <div className='flex'>
               <Image src="/icon/phone.svg" alt="" width={17} height={17} />
               <a href="tel:+919825687734">+91 98256 87734</a><a href="tel:+919586078607">+91 95860 78607</a>
             </div>
-            <div className='d-flex'>
+            <div className='flex'>
               <Image src="/icon/email.svg" alt="" width={18.85} height={13.25} />
               <a href="mailto:mahashaktiplast.0607@gmail.com">mahashaktiplast.0607@gmail.com</a>
             </div>
           </div>
           <div className='footer_data'>
             <h3>Business Hours</h3>
-            <div className='d-flex'>
+            <div className='flex'>
               <b>Monday-Saturday</b> 
               <p>10am to 6pm</p>
             </div>
-            <div className='d-flex'>
+            <div className='flex'>
               <b>Sunday</b> 
               <p>Closed</p>
             </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.css'
+import '/styles/globals.css'
 import '/styles/index.scss'
 
 function MyApp({ Component, pageProps }) {
